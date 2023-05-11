485966e3b821076598e57d367644bd181472608f
AssanCred <temix@live.ru>
2023-05-11 10:36:07 +0700

    trying to get data

---
+++ Rick&Morty_API_App/Storyboards/CharacterTableViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CharacterTableViewController.swift
+//  Rick&Morty_API_App
+//
+//  Created by Артём Латушкин on 11.05.2023.
+//
+
+import UIKit
+
+class CharacterTableViewController: UITableViewController {
+    
+    private let networkManager = NetworkManager.shared
+    private var persons: [Character] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Characters"
+        fetchCourse()
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        persons.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellCharacter", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        let person = persons[indexPath.row]
+        content.text = person.name
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+
+
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
+
+// MARK: - Networking
+extension CharacterTableViewController {
+    
+    private func fetchCourse() {
+        networkManager.fetchCharacter(from: Link.characterLink.url) { result in
+            switch result {
+            case .success(let character):
+                self.persons = character.results
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
